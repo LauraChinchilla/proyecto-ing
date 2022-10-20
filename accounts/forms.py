5f9e9521adb3af12506898704a8b2f7418f52e9a
Davidcpu <daviddurandz67@gmail.com>
2022-10-19 18:44:19 -0600

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,18 +13,35 @@ class RegistrationForm(forms.ModelForm):
         'class': 'form-control',
     }))
 
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder':'Ingrese Nombre',
+        'class': 'form-control',
+    }))
+
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder':'Ingrese Apellidos',
+        'class': 'form-control',
+    }))
+
+    phone_nomber = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder':'Ingrese No. de Telefono',
+        'class': 'form-control',
+    }))
+
+    email = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder':'Ingrese Email',
+        'class': 'form-control',
+    }))
+
     class Meta:
         model = Account
         fields = ['first_name','last_name', 'phone_nomber', 'email', 'password']
 
 
     def __int__(self, *args, **kwargs):
-        super(RegistrationForm, self).__int__(*args, **kwargs)
+        super(RegistrationForm, self).__int__(*args,**kwargs)
         #ESTA PARTE NO FUNCIONA
-        self.fields['first_name'].widget.attrs['placeholder'] = 'Ingrese Nombre'
-        self.fields['last_name'].widget.attrs['placeholder']='Ingrese Apellidos'
-        self.fields['phone_nomber'].widget.attrs['placeholder']='Ingrese No. de Telefono'
-        self.fields['email'].widget.attrs['placeholder']='Ingrese Email'
+
         for field in self.fields:
             self.fields[field].widget.attrs['class']='form-control'
 
