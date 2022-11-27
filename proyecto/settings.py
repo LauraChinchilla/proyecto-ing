745513cc104017c7243adcca9e391f835cc885ad
LauraChinchilla <karichinchilla@hotmail.com>
2022-11-26 21:02:44 -0600

    Update settings.py

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_session_timeout.middleware.SessionTimeoutMiddleware',
+
 ]
 
 SESSION_EXPIRE_SECONDS = 1000
@@ -157,5 +158,3 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
