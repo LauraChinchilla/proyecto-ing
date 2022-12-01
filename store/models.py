848797aa04f57f35f36ab976fbfdfc28fae60733
LeonardoBaca10 <115771234+LeonardoBaca10@users.noreply.github.com>
2022-11-30 23:17:40 -0600

    Update models.py

--- store/models.py
+++ store/models.py
@@ -35,7 +35,7 @@ class Product(models.Model):
     stock = models.IntegerField()
     is_available = models.BooleanField(default = True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    usuario = models.ForeignKey(Account, on_delete=models.CASCADE)
+    #usuario = models.ForeignKey(Account, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
