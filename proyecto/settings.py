28a84f41ea443f9aff51fc14fb1420b05a38daf4
LeonardoBaca10 <115771234+LeonardoBaca10@users.noreply.github.com>
2022-12-02 21:04:49 -0600

    Update settings.py

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-DEBUG = config('DEBUG', cast=bool, default=True)#para el heroku
+DEBUG = config('DEBUG', cast=bool, default=False)#para el heroku
 
 ALLOWED_HOSTS = ['*'] #heroku
 
